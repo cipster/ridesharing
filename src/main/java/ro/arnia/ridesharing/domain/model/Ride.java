9128574b4b840790f70aefc6b19f5ab1bb2f4699
florin horopciuc <florin.horopciuc@arnia.ro>
2018-07-19 13:23:21 +0300

    added Ride entity

--- src/main/java/ro/arnia/ridesharing/domain/model/Ride.java
+++ src/main/java/ro/arnia/ridesharing/domain/model/Ride.java
@@ -1,4 +1,16 @@
 package ro.arnia.ridesharing.domain.model;
 
-public class Ride {
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public class Ride extends BaseEntity{
+
+    private Car usedCar;
+    private Set<User> passengers;
+    private Location startPoint;
+    private Location endPoint;
+    private List<Location> stops;
+    private Date startDate;
+
 }
