2b20c0d7e57d3d14e0f472e384d3b57966355399
florin horopciuc <florin.horopciuc@arnia.ro>
2018-08-01 14:26:03 +0300

    Implemented ApplicationConfig for Mongo

---
+++ src/main/java/ro/arnia/ridesharing/config/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package ro.arnia.ridesharing.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Configuration
+//@EnableMongoRepositories
+class ApplicationConfig extends AbstractMongoConfiguration {
+
+  @Override
+  protected String getDatabaseName() {
+    return "test";
+  }
+
+  @Override
+  public MongoClient mongoClient() {
+    return new MongoClient();
+  }
+
+  @Override
+  protected Collection<String> getMappingBasePackages() {
+    return Arrays.asList("ro.arnia.ridesharing.domain.model.repository");
+  }
+}
